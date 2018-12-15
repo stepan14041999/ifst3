f2239687b45d6667c65957e88980db9aef8f52ca
alexandra <mail.to.register.011018@gmail.com>
2018-12-15 17:51:31 +0400

    after

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,39 +1,49 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="main">
-    <div class="container-fluid content">
-        <div class="col-md-2 bg-danger">
-            <h3>Категории:</h3>
-            <div class="list-group">
-                <button class="list-group-item list-group-item-action">Категория1</button>
-                <button class="list-group-item list-group-item-action">Категория1</button>
-                <button class="list-group-item list-group-item-action">Категория1</button>
-                <button class="list-group-item list-group-item-action">Категория1</button>
-                <button class="list-group-item list-group-item-action">Категория1</button>
-                <button class="list-group-item list-group-item-action">Категория1</button>
-            </div>
-        </div>
-        <div class="col-md-8 bg-info">
-            <h3>Товары: </h3>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-10">
             <div class="row">
-                <div class="col-md-3">
-                    <div class="card">
-                        <img class="card-img-top" src="{{asset('images/2kp3scovq0owgk48844ww4scg.jpg')}}" alt="Card image cap">
-                        <div class="card-body">
-                            <h5 class="card-title">Товар1</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        </div>
+                <div class="col-md-3 text-light" style="background-color: #2F2415;">
+                    <h3>Категории:</h3>
+                    <div class="list-group">
+                        <button class="list-group-item list-group-item-action">Категория1</button>
+                        <button class="list-group-item list-group-item-action">Категория1</button>
+                        <button class="list-group-item list-group-item-action">Категория1</button>
+                        <button class="list-group-item list-group-item-action">Категория1</button>
+                        <button class="list-group-item list-group-item-action">Категория1</button>
+                        <button class="list-group-item list-group-item-action">Категория1</button>
                     </div>
                 </div>
-                <div class="col-md-3"></div>
-                <div class="col-md-3"></div>
-                <div class="col-md-3"></div>
-                <div class="col-md-3"></div>
-                <div class="col-md-3"></div>
+                <div class="col-md-9" style="overflow: auto; height: 800px; background-color: #E9DFD3;">
+                    <h3>Товары: </h3>
+                    <div class="row">
+                        <?php for ($i=0;$i<8;$i++){
+                            ?>
+                            <div class="col-md-4 mb-4">
+                                <div class="card">
+                                    <div class="img-block" style="height: 200px; text-align: center">
+                                        <img class="h-100 w-auto" src="{{asset('images/2kp3scovq0owgk48844ww4scg.jpg')}}" alt="">
+                                    </div>
+
+                                    <div class="card-body">
+                                        <h5 class="card-title">Товар1</h5>
+                                        <p class="card-text">Price$</p>
+                                        <button class="btn btn-primary w-100">Подробнее</button>
+                                    </div>
+                                </div>
+                            </div>
+<?php
+                        }?>
+
+
+                    </div>
+                </div>
             </div>
         </div>
-        <div class="col-md-2 bg-success">3</div>
+
+        <div class="col-md-2" style="background-color: #342B1E;">3</div>
     </div>
 </div>
 
--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,7 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/main', function (){
    return view('main');
 });
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
